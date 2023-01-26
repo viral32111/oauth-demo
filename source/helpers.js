9224ed304b7b21ea40ab96e274bdc8fb4f86d4f2
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-01-26 00:26:57 +0000

    Start from scratch using TypeScript

--- source/helpers.js
+++
@@ -1,8 +0,0 @@
-export const sha1 = async ( value ) => {
-	const digest = await crypto.subtle.digest( "SHA-1", new TextEncoder().encode( value ) )
-	return Array.from( new Uint8Array( digest ) ).map( byte => byte.toString( 16 ).padStart( 2, "0" ) ).join( "" )
-}
-
-export const isSignedIn = ( session ) => {
-	return "user_id" in session && session[ "user_id" ] !== ""
-}
